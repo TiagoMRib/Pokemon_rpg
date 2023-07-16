05bce310cd2e7171c07758d56f60f65d16a08cd8
TiagoMRib <tiago18.2002@gmail.com>
2023-07-16 12:30:01 +0100

    Basic world and pokemon added

---
+++ Assets/Scripts/Pokemons/Pokemon.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pokemon 
+{
+    PokemonBase pkBase;
+    int level;
+
+    public Pokemon(PokemonBase pBase, int pLevel)
+    {
+        pkBase = pBase;
+        level = pLevel;
+
+    }
+
+
+    public int Attack {
+        get { return Mathf.FloorToInt((pkBase.Attack * level) / 100f) + 5;}
+    }
+
+    public int Defense {
+        get { return Mathf.FloorToInt((pkBase.Defense * level) / 100f) + 5;}
+    }
+
+    public int SpecialAttack {
+        get { return Mathf.FloorToInt((pkBase.SpecialAttack * level) / 100f) + 5;}
+    }
+
+    public int SpecialDefense {
+        get { return Mathf.FloorToInt((pkBase.SpecialDefense * level) / 100f) + 5;}
+    }
+
+    public int Speed {
+        get { return Mathf.FloorToInt((pkBase.Speed* level) / 100f) + 5;}
+    }
+
+    public int MaxHP {
+        get { return Mathf.FloorToInt((pkBase.MaxHP * level) / 100f) + 10;}
+    }
+
+    
+}
---
+++ Assets/Scripts/Pokemons/PokemonBase.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// new option in  "Create" to add a pokemon to the game
+[CreateAssetMenu(fileName = "Pokemon", menuName = "Pokemon/Create new Pokemon")]
+
+public class PokemonBase : ScriptableObject
+{
+
+    [SerializeField] string pkName;
+    [TextArea] [SerializeField] string description;
+
+    [SerializeField] Sprite frontSprite;
+    [SerializeField] Sprite backSprite;
+
+    [SerializeField] PokemonType type1;
+    [SerializeField] PokemonType type2;
+
+    // BASE STATS
+    [SerializeField] int maxHP;
+
+    [SerializeField] int attack;
+    [SerializeField] int defense;
+
+    [SerializeField] int specialAttack;
+    [SerializeField] int specialDefense;
+
+    [SerializeField] int speed;
+
+
+    public string Name{
+        get{ return pkName;}
+    }
+
+    public string Description
+    {
+        get { return description; }
+    }
+
+    public Sprite FrontSprite
+    {
+        get { return frontSprite; }
+    }
+
+    public Sprite BackSprite
+    {
+        get { return backSprite; }
+    }
+
+    public PokemonType Type1
+    {
+        get { return type1; }
+    }
+
+    public PokemonType Type2
+    {
+        get { return type2; }
+    }
+
+    public int MaxHP
+    {
+        get { return maxHP; }
+    }
+
+    public int Attack
+    {
+        get { return attack; }
+    }
+
+    public int Defense
+    {
+        get { return defense; }
+    }
+
+    public int SpecialAttack
+    {
+        get { return specialAttack; }
+    }
+
+    public int SpecialDefense
+    {
+        get { return specialDefense; }
+    }
+
+    public int Speed
+    {
+        get { return speed; }
+    }
+
+
+}
+
+
+public enum PokemonType
+{
+    None, Normal, Fire, Water, Electric, Grass, Ice, Fighting, Poison, Ground, Flying, Psychic, Bug, Rock, Ghost, Dragon, Dark, Steel, Fairy
+}
